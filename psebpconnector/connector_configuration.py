b54c2c3c02abc86f4a206e8f292a9ca6cf8edf28
Guillaume Chinal <guiiix@invisiblethingslab.com>
2024-11-02 14:42:56 +0100

    add: parse config file

---
+++ psebpconnector/connector_configuration.py
@@ -0,0 +1,40 @@
+"""
+MIT License
+
+Copyright (c) 2024 Foxchip
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+
+from configparser import ConfigParser, Error
+from pathlib import Path
+
+
+class ConnectorConfiguration:
+    def __init__(self, config_path: Path):
+        if not config_path.is_file():
+            raise FileNotFoundError(f"The file {config_path} does not exist.")
+
+        self.config = ConfigParser()
+
+        try:
+            self.config .read(config_path)
+        except Error as e:
+            raise ValueError(f"Error reading the configuration file: {e}")
