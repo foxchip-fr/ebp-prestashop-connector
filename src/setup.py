754d4e019e366c9d8f166d04b88692e280ebf761
Guillaume Chinal <guiiix@invisiblethingslab.com>
2024-11-26 23:57:21 +0100

    fix: CI

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='psebpconnector',
+    version='0.0.1',
+    url='https://github.com/foxchip-fr/ebp-prestashop-connector',
+    author='Guillaume Chinal',
+    author_email='contact@guillaumechinal.fr',
+    description='Lightweight connector that synchronizes Prestashop orders with EBP Gestion Commerciale ',
+    packages=find_packages(),
+    install_requires=['O365 >= 2, < 3', 'requests >= 2, < 3'],
+)
